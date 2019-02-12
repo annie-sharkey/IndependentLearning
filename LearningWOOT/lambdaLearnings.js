d04a6d2f0e089d171a884410a5b365bbc45ae528
Annie Sharkey <anniesharkey@students-mbp-4.lan>
2019-02-12 13:49:39 -0500

    files

---
+++ LearningWOOT/lambdaLearnings.js
@@ -0,0 +1,33 @@
+/*
+Lambda Calculus proivdes a way to _____ using three things.
+
+Lambda Calculus uses only functions, no data types, to represent fundamental features of programming
+like numbers or boolean logic. 
+
+An expression in Lambda calculus can be one of three things: expression = variable || abstraction || application. 
+    1. Variable x is an expression itself
+    2. Abstraction = λ(variable).(expression). For example, λx.x.
+    3. Application ==> (expression1)(expression2)
+
+Let's dig into each of the items above.
+Variables are expressions themselves and are represented by x, y, z, etc.
+
+Think of an abstraction like the definition of a function. In λx.x:
+    λ signals "the following is a function!"
+    x (the x directly after λ and before .) is your argument that will be passed into the body...
+    .x is the body of your function. This is where arguments are entered and stuff happens/is returned.
+
+    Think of λx.x like: function stuff(x) {return x};
+
+An application where application = (expression)(expression) might consist of two functions
+or a function and a variable or even more than two functions with multiple variables. This
+is best understood with examples...
+
+-------------------------------
+
+
+
+
+
+
+*/
---
+++ Random/lambdaRandom.js
@@ -0,0 +1,62 @@
+// var elements = ["annie", "elizabeth", "freddy"];
+
+// var list = [];
+
+// elements.map(element => list.push(element.length));
+
+// console.log(list);
+
+// var last = elements[elements.length - 1];
+// console.log(last);
+
+// var newList = [];
+// elements.forEach(element => newList.push(element + "s"));
+// console.log(newList);
+
+// var newNewList = ["annie", 9, 100];
+// console.log(newNewList);
+
+// var ages = [1, 2, 3, 4, 5];
+// console.log(ages);
+// ages.pop();
+// console.log(ages);
+
+// for (index = 0; index < ages.length; index++) {
+//   ages[index] = ages[index] + 1;
+// }
+
+// ages.forEach(age => age + 1);
+
+// for (age in ages) {
+//   console.log(age);
+// }
+
+// console.log(ages);
+
+// ages.map(age => console.log(age * 3));
+
+// const E = y => (y ** 2)(4);
+// const A = 3;
+
+// console.log(E(A));
+
+// let x = 2;
+// let z = 4 * x;
+
+// const E = (x, a) => x - 1 + a;
+
+// console.log(E(z, 1));
+
+// const areaSquare = x => x ** 2;
+// const getFormula = formula => formula;
+
+// console.log(getFormula(areaSquare));
+
+const area = base => height => base * height;
+let height = 4;
+console.log(area(2)(height));
+
+// const annie = (x => y => x + y)(1)(2);
+// console.log(annie);
+
+// λx. y
