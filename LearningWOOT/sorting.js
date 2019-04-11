71f9a08185a1c5c176b82b1e299bf50ccbee73fd
Annie Sharkey <anniesharkey@d-172-26-41-127.dhcp.virginia.edu>
2019-04-10 22:58:19 -0400

    sorting

--- LearningWOOT/sorting.js
+++ LearningWOOT/sorting.js
@@ -23,7 +23,7 @@ complexity of bubbleSort function below:
     takes up O(n) space
 
 */
-const list = [6, 3, 10, 25, -100, 1000, 9, 0];
+const list = [6, 3, 10, 9, 25, -100, 1000, 9, 0, 87];
 
 const bubbleSort = list => {
   var swapMade = false;
@@ -61,6 +61,8 @@ method:
     2. recombine and sort at same time
     3. sort by comparing the left most items and then adding the lesser value
     back first
+
+time complexity: O(nlogn)
 */
 
 //can we recursively split?
@@ -81,23 +83,114 @@ const mergeSort = list => {
 const merge = (left, right) => {
   var result = [];
 
-  console.log("left " + left);
-  console.log("right" + right);
-  while (left.length || right.length) {
-    //if no more elements in the right list
-    if (!right.length) {
+  //   console.log("left " + left);
+  //   console.log("right " + right);
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
       result.push(left.shift());
-    } else if (!left.length) {
-      result.push(right.shift());
     } else {
-      if (left[0] < right[0]) {
-        result.push(left.shift());
-      } else {
-        result.push(right.shift());
-      }
+      result.push(right.shift());
     }
   }
+
+  //left has smaller values so go first
+  while (left.length) {
+    result.push(left.shift());
+  }
+
+  while (right.length) {
+    result.push(right.shift());
+  }
+
+  return result;
 };
 
-console.log(mergeSort(list));
-console.log(list);
+// console.log(mergeSort(list));
+
+/*
+******QUICKSORT********
+Divide and conquer
+Pick a pivot point ==> pick at end
+    Then partition list so that all elements less than end are to the left
+    and all elements greater than end are to the right
+
+    Move the greater number to the right of your pivot
+    Move pivot down one index but there's a number there
+    Move that number to where the moved number originally was
+
+    Once first iteration done, pivot is in its final spot
+    Then sort the left and the right, excluding the pivot
+
+*/
+
+list3 = [5, 2, 8, 1, 2, 4, 3, 7, 1, 6];
+//check issue with duplicates sometimes causing problems
+const quicksort = list => {
+  if (list.length <= 1) {
+    return list;
+  }
+
+  var pivotIndex = list.length - 1;
+  var currentIndex = 0;
+
+  while (list[currentIndex] != list[pivotIndex]) {
+    if (list[currentIndex] < list[pivotIndex]) {
+      currentIndex++;
+    } else if (list[currentIndex] > list[pivotIndex]) {
+      const leftOfPivot = list[pivotIndex - 1];
+      const pivotValue = list[pivotIndex];
+      const moveLargeNumtoRight = list[currentIndex];
+
+      list[currentIndex] = leftOfPivot;
+      list[pivotIndex - 1] = pivotValue;
+      list[pivotIndex] = moveLargeNumtoRight;
+
+      pivotIndex--;
+    }
+  }
+
+  //   console.log(list.slice(0, pivotIndex).concat(list[pivotIndex]));
+  return quicksort(list.slice(0, pivotIndex))
+    .concat(list[pivotIndex])
+    .concat(quicksort(list.slice(pivotIndex + 1)));
+};
+
+// console.log(quicksort(list3));
+// console.log(quicksort(list));
+
+/*
+***SELECTION SORT****
+scan list for lowest value
+put the lowest value in the beginning
+move the current beginning to where the old num was
+move through list
+*/
+
+var woot = [2, 5, 1, 7, 3, 6, 4];
+
+const selectionSort = list => {
+  //try with length of lift and shifting
+
+  
+  //   var minIndex = 0;
+  //   var needToSwap = false;
+  //   for (var x = 0; x < list.length; x++) {
+  //     for (let i = 1 + x; i < list.length; i++) {
+  //       if (list[i] < list[minIndex]) {
+  //         minIndex = i;
+  //         needToSwap = true;
+  //       }
+  //     }
+  //     if (needToSwap) {
+  //       const minElement = list[minIndex];
+  //       const elementToMoveToLater = list[x];
+  //       list[x] = minElement;
+  //       list[minIndex] = elementToMoveToLater;
+  //     }
+  //     // console.log(list);
+  //   }
+  //   return list;
+};
+
+console.log(selectionSort(woot));
